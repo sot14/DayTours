8aeb1813319d7d6c56385d3156f662f63ea23d3e
annamaria99 <ams58@hi.is>
2020-03-19 10:52:50 +0000

    getters og setters á review og tour

--- src/Daytours/Tour.java
+++ src/Daytours/Tour.java
@@ -12,5 +12,75 @@ public class Tour {
     int participantNum;
     Review[] review;
 
+    public String getCompany() {
+        return company;
+    }
 
+    public double getLength() {
+        return length;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String[] getLocation() {
+        return location;
+    }
+
+    public String getTourInfo() {
+        return tourInfo;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getTourID() {
+        return tourID;
+    }
+
+    public int getParticipantNum() {
+        return participantNum;
+    }
+
+    public Review[] getReview() {
+        return review;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setLocation(String[] location) {
+        this.location = location;
+    }
+
+    public void setTourInfo(String tourInfo) {
+        this.tourInfo = tourInfo;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setTourID(int tourID) {
+        this.tourID = tourID;
+    }
+
+    public void setParticipantNum(int participantNum) {
+        this.participantNum = participantNum;
+    }
+
+    public void setReview(Review[] review) {
+        this.review = review;
+    }
 }
