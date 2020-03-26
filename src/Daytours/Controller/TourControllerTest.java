1ab8a3cfd92de93fd90e840c23a0cdec63e338e7
annamaria99 <ams58@hi.is>
2020-03-26 22:03:11 +0000

    Bjó til test skrá

---
+++ src/Daytours/Controller/TourControllerTest.java
@@ -0,0 +1,18 @@
+package Daytours.Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TourControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void getTour() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllTours() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchTour() {
+    }
+}
