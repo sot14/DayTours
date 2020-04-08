314ccdfc15033d5b89360e70efb5329d9c17465e
gudlaugagnes <gak16@hi.is>
2020-04-08 14:40:04 +0000

    index síða, filterar, bæting í database, tourcontroller og indexsitecontroller

--- src/Daytours/Controller/TourController.java
+++ src/Daytours/Controller/TourController.java
@@ -2,7 +2,9 @@ package Daytours.Controller;
 import Daytours.Model.Tour;
 import Daytours.Database.DataBaseManager;
 
+
 import java.util.ArrayList;
+import java.sql.Date;
 
 
 public class TourController {
@@ -21,6 +23,19 @@ public class TourController {
     public ArrayList<Tour> getAllTours(){
         return db.getAllTours();
     }
+
+    public ArrayList<Tour> getAllToursWithMaxLength(int len){
+        return db.getAllToursWithMaxLength(len);
+    }
+
+    public ArrayList<Tour> getAllToursWithLocation(String loc){
+        return db.getAllToursWithLocation(loc);
+    }
+
+    public ArrayList<Tour> getAllToursInsideTimePeriod(Date start, Date end){ return db.getAllToursInsideTimePeriod(start,end); }
+
+    public ArrayList<Tour> getAllToursCheaper(int price) { return db.getAllToursCheaper(price); }
+
 
     public ArrayList<Tour> searchTour(String tourName) {
         return db.searchTourName(tourName);
--- src/Daytours/UI/IndexSiteController.java
+++ src/Daytours/UI/IndexSiteController.java
@@ -2,6 +2,7 @@ package Daytours.UI;
 import Daytours.Controller.TourController;
 import Daytours.Model.Tour;
 
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,22 +11,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.sql.Date;
 
 public class IndexSiteController implements Initializable {
 
-
-    private TourController tourController;
-
+    @FXML
+    public Slider lengdSlider;
+    @FXML
+    public ComboBox landshlutiCombobox;
+    @FXML
+    public DatePicker tilDatePicker;
+    @FXML
+    public DatePicker fraDatePicker;
+    @FXML
+    public Slider verdSlider;
     @FXML
     private ListView <Tour> tourList;
     @FXML
@@ -33,18 +43,23 @@ public class IndexSiteController implements Initializable {
     @FXML
     public Button veljaFerdButton;
 
+    private TourController tourController;
     int tourID;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         tourController = new TourController();
         setTourList();
+        landshlutiCombobox.getItems().removeAll(landshlutiCombobox.getItems());
+        landshlutiCombobox.getItems().addAll("Allt landið", "Höfuðborgarsvæðið", "Vesturland", "Vestfirðir", "Norðurland", "Austurland", "Suðurland");
+        landshlutiCombobox.getSelectionModel().select("Allt landið");
     }
 
     public void setTourList(){
         ArrayList<Tour> listOfTours = tourController.getAllTours();
         ObservableList<Tour> listViewTours = FXCollections.observableArrayList(listOfTours);
         tourList.setItems(listViewTours);
+        tourList.setFixedCellSize(60.0);
     }
 
     public void updateTour(ObservableList<Tour> obs) {
@@ -75,12 +90,61 @@ public class IndexSiteController implements Initializable {
 
     }
 
+
     public void leitaFerdHandler(ActionEvent actionEvent) {
         String l = leitaFerd.getText();
         ArrayList<Tour> listOfTours = tourController.searchTour(l);
         updateTour(FXCollections.observableArrayList(listOfTours));
     }
 
+    // atburðarhandler á sleðanum sem notaður er til að velja hámarkslengd ferða
+    public void lengdSliderHandler(MouseEvent mouseEvent) {
+        int chosenLength = (int)lengdSlider.getValue();
+        ArrayList<Tour> listOfTours = tourController.getAllToursWithMaxLength(chosenLength);
+        ObservableList<Tour> listViewTours = FXCollections.observableArrayList(listOfTours);
+        tourList.setItems(listViewTours);
+    }
 
+    // atburðahandler fyrir hvaða landshluti er valinn
+    public void landshlutiHandler(ActionEvent actionEvent) {
+        String chosenLandshluti = landshlutiCombobox.getSelectionModel().getSelectedItem().toString();
+        if(chosenLandshluti.equals("Allt landið")) {
+            setTourList();
+        }
+        else {
+            ArrayList<Tour> listOfTours = tourController.getAllToursWithLocation(chosenLandshluti);
+            ObservableList<Tour> listViewTours = FXCollections.observableArrayList(listOfTours);
+            tourList.setItems(listViewTours);
+        }
+    }
 
+    // atburðarhandler fyrir hvaða dagsetninga tímabil er valið
+    public void timabilHandler(ActionEvent actionEvent) {
+        LocalDate fraDate = fraDatePicker.getValue();
+        LocalDate tilDate = tilDatePicker.getValue();
+
+        // passa að búið sé að velja bæði frá og til dagsetningu
+        if(fraDate != null && tilDate!=null) {
+            if (fraDate.isAfter(tilDate)) {
+                System.out.println("*** Ólöglegt val á tímabili. *** ");
+                System.out.println("Byrjunardagsetning á tímabilinu kemur á eftir endadagsetningu tímabilsins.");
+            }
+            else {
+                Date fraDatesql = Date.valueOf(fraDate);
+                Date tilDatesql = Date.valueOf(tilDate);
+                ArrayList<Tour> listOfTours = tourController.getAllToursInsideTimePeriod(fraDatesql,tilDatesql);
+                ObservableList<Tour> listViewTours = FXCollections.observableArrayList(listOfTours);
+                tourList.setItems(listViewTours);
+            }
+        }
+
+    }
+
+    public void verdSliderHandler(MouseEvent mouseEvent) {
+        int chosenPrice = (int)verdSlider.getValue();
+        System.out.println(chosenPrice);
+        ArrayList<Tour> listOfTours = tourController.getAllToursCheaper(chosenPrice);
+        ObservableList<Tour> listViewTours = FXCollections.observableArrayList(listOfTours);
+        tourList.setItems(listViewTours);
+    }
 }
